b5be44d7626ff9a7effbf2eb3106e52618dd4fc4
Wael Sulais <s202032440@kfupm.edu.sa>
2022-04-01 01:14:49 +0300

    feat: Initial commit

---
+++ src/main/java/edu/kfupm/swe206/App.java
@@ -0,0 +1,27 @@
+package edu.kfupm.swe206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
