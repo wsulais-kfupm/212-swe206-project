90916ea2c9a8e2550c433334f0d92641648f8971
naifAlbalawi <84006088+naifAlbalawi@users.noreply.github.com>
2022-04-25 18:11:15 +0300

    naif albalawi

--- src/main/java/edu/kfupm/swe206/model/JobOffer.java
+++ src/main/java/edu/kfupm/swe206/model/JobOffer.java
@@ -1,4 +1,8 @@
 package edu.kfupm.swe206.model;
+
+
+import jdk.internal.net.http.common.Pair;
+
 import java.util.List;
 import java.util.Set;
 
@@ -36,7 +40,55 @@ public class JobOffer{
         this.benefits=benefits;
     }
 
+    public Unit getUnit() {
+        return unit;
+    }
 
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
 
+    public JobPosition getPosition() {
+        return position;
+    }
 
+    public void setPosition(JobPosition position) {
+        this.position = position;
+    }
+
+    public double getLowerSalary() {
+        return lowerSalary;
+    }
+
+    public void setLowerSalary(double lowerSalary) {
+        this.lowerSalary = lowerSalary;
+    }
+
+    public double getHigherSalary() {
+        return higherSalary;
+    }
+
+    public void setHigherSalary(double higherSalary) {
+        this.higherSalary = higherSalary;
+    }
+
+    public double getOfferedSalary() {
+        return offeredSalary;
+    }
+
+    public void setOfferedSalary(double offeredSalary) {
+        this.offeredSalary = offeredSalary;
+    }
+
+    public Set<Benefit> getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(Set<Benefit> benefits) {
+        this.benefits = benefits;
+    }
+
+    public Pair<Double, Double> getSalaryRange(){
+        return new Pair<Double, Double>(lowerSalary,higherSalary);
+    }
 }
